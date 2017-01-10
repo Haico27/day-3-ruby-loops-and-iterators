f84e4a840774ad437023760f10922c456f115092
Haico27 <haico.wensink@gmail.com>
2017-01-10 15:21:32 +0100

    Define the Ingredients

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -9,3 +9,14 @@ def print_progress_bar
 end
 
 print_divider
+
+NUM_POTATOES = 5
+NUM_EGGS = 6
+
+ingredients = [
+  {name: 'potatoes', quantity: NUM_POTATOES}
+  {name: 'eggs', quantity: NUM_EGGS}
+  {name: 'onion', quantity: 1 }
+  {name: 'oil', quantity: 'some'}
+  {name: 'salt', quantity: 'some'}
+]
