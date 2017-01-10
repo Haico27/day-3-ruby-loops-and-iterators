b7d4d8380c6c39dcebb6c7d39fd3a9095a755e0c
Haico27 <haico.wensink@gmail.com>
2017-01-10 15:34:34 +0100

    Print out the Ingredients

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -8,15 +8,24 @@ def print_progress_bar
   puts "\n"
 end
 
-print_divider
 
 NUM_POTATOES = 5
 NUM_EGGS = 6
 
 ingredients = [
-  {name: 'potatoes', quantity: NUM_POTATOES}
-  {name: 'eggs', quantity: NUM_EGGS}
-  {name: 'onion', quantity: 1 }
-  {name: 'oil', quantity: 'some'}
-  {name: 'salt', quantity: 'some'}
+  { name: 'potatoes', quantity: NUM_POTATOES },
+  { name: 'eggs', quantity: NUM_EGGS },
+  { name: 'onion', quantity: 1 },
+  { name: 'oil', quantity: 'some' },
+  { name: 'salt', quantity: 'some' },
 ]
+
+puts "Let's make that omelette!"
+
+print_divider
+
+puts "You need the following ingredients: "
+
+ingredients.each do |ingredient|
+  puts "* #{ingredient[:quantity]} #{ingredient[:name]}"
+end
